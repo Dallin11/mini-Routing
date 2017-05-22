d7d86bb2f0c54f826a21d961816c7420d8d34906
Your Name <you@example.com>
2017-05-22 13:39:51 -0600

    mini project

---
+++ app.js
@@ -0,0 +1,11 @@
+angular.module("app", ["ui.router"])
+.config(function($stateProvider, $urlRouterProvider){
+        $urlRouterProvider.when('')
+
+    $stateProvider
+    .state("home", {
+        url: '/',
+        templateUrl: "home/home.html",
+        controller: "homeCtrl"
+    })
+})
